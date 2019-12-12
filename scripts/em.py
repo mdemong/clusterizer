56f763f180526b43cdfbe6dadf109a3c11670510
mdemong <mdemong1@gmail.com>
2019-12-11 16:04:38 -0800

    fixed up init_random_distributions a bit

--- scripts/em.py
+++ scripts/em.py
@@ -1,11 +1,16 @@
 from scipy.stats import norm
 import numpy as np
+from pprint import pprint as pprint
 # NOTE: To run this code, you may need to run: 
 #       `pip install scipy`
 #       `pip install numpy` 
 #       if you have not already installed numpy and scipy.
+# I'm using numpy v1.17
 
-# It is recommended to use numpy arrays for efficiency. See https://stackoverflow.com/a/994010
+# When using numeric values, it is recommended to use numpy arrays for efficiency. 
+# See https://stackoverflow.com/a/994010
+# One drawback of this is that we have to explicitly define array size during initialization,
+# rather than having dynamically changing array size.
 
 
 # TODO: define function signature
@@ -24,10 +29,11 @@ def read_input():
 # Performs the Expectation Maximization algorithm as described in:
 # Clustering for malware classification; Pai et al. (2016)
 # 
-def expectation_maximization(k, temp_points, distributions=np.array()):
+def expectation_maximization(k, temp_points, distributions=[]):
     # Initialize parameters (mean, variance)
 
-    if(distributions==[]):
+    # I'm using standard python lists for distributions, since distributions objects aren't numbers
+    if(distributions == []):
         distributions = init_random_distributions(k)
 
     # Alternate between E and M step until change is negligable
@@ -48,19 +54,33 @@ def expectation_maximization(k, temp_points, distributions=np.array()):
 
 
 def init_random_distributions(count):
-    # For now, assuming we can have any float as a mean or SD
-    MIN_RANDOM_MEAN = np.finfo.min
-    MAX_RANDOM_MEAN = np.finfo.max
-    
-    MIN_RANDOM_SD = np.finfo.min
-    MAX_RANDOM_SD = np.finfo.max
+    # For now, assuming we can have any float as a mean or SD.
 
-    dists = np.array()
+    # TODO: These values are somewhat arbitrary. Do we need to adjust them?
+    RANDOM_MEAN_RANGE = 5
+    MIN_RANDOM_MEAN = -2.5
+    
+    MAX_RANDOM_SD = 5
+    # Standard deviations are always positive!
+    # Setting minimum to epsilon to keep it nonzero 
+    # (who knows what that might do...)
+    MIN_RANDOM_SD = np.finfo(float).eps
+
+    dists = []
     for i in range(count):
-        mean = np.random.uniform(low=MIN_RANDOM_MEAN, high=MAX_RANDOM_MEAN)
-        sd = np.random.uniform(low=MIN_RANDOM_SD, high=MAX_RANDOM_SD)
+        mean = np.random.default_rng().random()
+        mean = (mean * RANDOM_MEAN_RANGE) + MIN_RANDOM_MEAN
+
+        # sd = np.random.default_rng().uniform(MIN_RANDOM_SD, MAX_RANDOM_SD)
+        sd = np.random.default_rng().random()
+        sd = (sd * MAX_RANDOM_SD) + MIN_RANDOM_SD
+
         new_dist = norm(mean, sd)
-        dists = np.append(dists, new_dist)
+        # TODO: remove these debug print statements
+        print("mean:", new_dist.mean())
+        print("sd  :", new_dist.std())
+        print()
+        dists.append(new_dist)
     return dists
 
 
