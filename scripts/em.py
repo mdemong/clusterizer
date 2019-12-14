2b00397cbd28ccfdc6420e834c8dd51fbe5bcba9
mdemong <mdemong1@gmail.com>
2019-12-14 14:27:02 -0800

    implemented normalized mean difference stop condition

--- scripts/em.py
+++ scripts/em.py
@@ -5,7 +5,7 @@ from pprint import pprint as pprint
 #       `pip install scipy`
 #       `pip install numpy` 
 #       if you have not already installed numpy and scipy.
-# I'm using numpy v1.17
+# I'm using numpy v1.17.
 
 # When using numeric values, it is recommended to use numpy arrays for efficiency. 
 # See https://stackoverflow.com/a/994010
@@ -32,15 +32,22 @@ def read_input():
 def expectation_maximization(k, temp_points, distributions=[]):
     # Initialize parameters (mean, variance)
 
-    # I'm using standard python lists for distributions, since distributions objects aren't numbers
+    # I'm using standard python lists for distributions;
+    # since distribution objects aren't numbers, they can't be put in numpy arrays.
     if(distributions == []):
         distributions = init_random_distributions(k)
 
+    old_means = get_means(distributions)
+
+    # Using an arbitrary multiplier to ensure we enter loop
+    # (because python doesn't have do-while loops)
+    ARBITRARY_MULTIPLIER = 1.1
+    new_means = [mean * ARBITRARY_MULTIPLIER for mean in old_means]
+
     # Alternate between E and M step until change is negligable
-    changed = True
-    while(changed):
+    while(significant_mean_change(old_means, new_means)):
+        old_means = new_means
         # TODO: modify 'changed' based on exp. and max. function outputs
-        changed = False
 
         # E step: Compute probabilities needed in M step, based on
           # current estimates of the distribution parameters
@@ -50,7 +57,29 @@ def expectation_maximization(k, temp_points, distributions=[]):
           # the distribution parameters, based on 
           # maximum likelihood estimators
         maximization()
+        new_means = get_means(distributions)
     return []
+
+# Using mean with difference 1e-6 as suggested in:
+# Analysis of stopping critera for the EM algorithm, Abbi et al.
+#
+# Using this for simplicity of implementation, even though using variance 
+# would be a more accurate heuristic.
+def significant_mean_change(old_means, new_means):
+
+    MINIMUM_CHANGE = 1e-6
+
+    for i, mean in enumerate(old_means):
+        normalized_diff = abs((old_means[i] - new_means[i]) / old_means[i])
+        if normalized_diff > MINIMUM_CHANGE:
+            return True
+
+    # If we get here, all of our diffs are small
+    return False
+
+
+def get_means(dist_list): 
+    return [dist.means() for dist in dist_list]
 
 
 def init_random_distributions(count):
@@ -92,3 +121,5 @@ def expectation():
 # TODO: define function signature
 def maximization():
     return
+
+
