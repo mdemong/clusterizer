fixed covariance, partway through maximization

scripts/em.py
@@ -1,6 +1,19 @@
 from scipy.stats import multivariate_normal
 import numpy as np
 from pprint import pprint as pprint
+
+## Expectation Maximization with Gaussian Mixture Models
+## Micah Demong
+##
+## The mathematical formulas used in this code are 
+## adapted from an online lecture by Alexander Ihler:
+## "Gaussian Mixture Models and EM"
+## https://youtu.be/qMTuMa86NzU
+##
+## Suggestions for the stopping criteria found in:
+## "Analysis of stopping critera for the EM algorithm", Abbi et al.
+
+
 # NOTE: To run this code, you may need to run: 
 #       `pip install scipy`
 #       `pip install numpy` 
@@ -67,10 +80,11 @@ def expectation_maximization(k, points, distributions=[]):
         prob_mtx = expectation(distributions, points)
 
         # M Step: Use the probabilities from the E step to recompute
-          # the distribution parameters, based on 
-          # maximum likelihood estimators
-        maximization()
+          # the distribution parameters, based on maximum likelihood estimators
+        distributions = maximization(prob_mtx, points)
+
         new_means = get_means(distributions)
+        
     return []
 
 
@@ -97,8 +111,10 @@ def get_means(dist_list):
 
 
 def init_random_distributions(count, points):
+    # TODO: this function could be improved, perhaps by random sampling
 
-    # TODO: this could be improved, perhaps by random sampling
+    dimension = len(points[0])
+    print(dimension)
 
     # gives an array [x_1-avg, x_2-avg, ..., x_n-avg]
     point_avg = np.mean(points, axis=0)
@@ -106,19 +122,32 @@ def init_random_distributions(count, points):
     # This selection of point_avg / 4 is a bit arbitrary.
     MAX_AVG_PERTURBATION = point_avg / 4
     
-    # This selection of point avg / 2 is also kind of arbitrary
-    MAX_RAND_COV_ARR = MAX_AVG_PERTURBATION / 2
+    # This selection is also kind of arbitrary
+    MAX_RAND_COV_ARR = point_avg / 4
 
+    # TODO: redo covariance matrix init, if needed?
     # Results in an n x n covariance matrix
-    MAX_RANDOM_COV = np.transpose(MAX_RAND_COV_ARR) * MAX_RAND_COV_ARR
+    # MAX_RANDOM_COV = MAX_RAND_COV_ARR * np.eye(dimension)
+    # cov = np.cov(points)
+
+    
+    # INIT_COV = np.eye(dimension) 
+
+    # MIN_COV = np.eye(dimension) * 1e-12
 
     dists = []
-    dimension = len(point_avg)
 
     for i in range(count):
         new_dist = dict()
         new_dist['mean'] = point_avg + (np.random.default_rng().random(size=dimension) * MAX_AVG_PERTURBATION)
-        new_dist['cov'] = (np.random.default_rng().random(size=(dimension, dimension)) * MAX_RANDOM_COV)
+
+        # Random covariance defined by https://stackoverflow.com/a/619406
+        # tempcov = (np.random.default_rng().random(size=(dimension, dimension))) * MAX_RAND_COV_ARR
+        # tempcov = np.cov(np.random.default_rng().random(size=(dimension, dimension)))
+        # new_dist['cov'] = tempcov * np.transpose(tempcov)
+        new_dist['cov'] = np.cov(np.transpose(points))
+        print(new_dist['cov'])
+
         new_dist['weight'] = 1 / count
         dists.append(new_dist)
     return dists
@@ -136,8 +165,6 @@ def expectation(distributions, points):
 
 def point_cluster_probability(point, cluster, distributions):
 
-    # x = np.linspace(0, 5, 10, endpoint=False)
-
     if(len(distributions) == 0):
         raise ValueError("There must be at least one distribution")
 
@@ -147,14 +174,37 @@ def point_cluster_probability(point, cluster, distributions):
     for dist in distributions:
         denom += dist['weight'] * multivariate_normal.pdf(point, mean=dist['mean'], cov=dist['cov'])
 
+    if(denom == 0): return 0
+
     return (num / denom)
 
 
 # TODO: define function signature
-def maximization():
-    return
+def maximization(prob_mtx, points):
+    k = len(prob_mtx[0])
+    dists = []
+
+    # vector value: total responsibility of each cluster
+    responsibilities = np.sum(prob_mtx, axis=0)
+    # scalar value: sum of all total responsibilities
+    total_resp = np.sum(responsibilities)
+
+    maxed_means = max
+
+    for i in range(k):
+        resp = responsibilities[i]
+        new_dist = dict()
+        new_dist['weight'] = resp / total_resp
+        # new_dist['mean']
+
+        dists.append(new_dist)
+
+    return dists
 
 
+def maximize_mean(resp, prob_mtx, i, points):
+    return np.average(points, axis=0, weights=prob_mtx[:,i]) / resp
 
+# def maximize_cov():
 
 # TODO: Create function to convert numpy arrays to json-serializable python lists

scripts/test_em.py
@@ -1,22 +1,48 @@
+# You can run this with pytest:
+# `cd .../clusterizer/scripts`
+# `pip install -U pytest`
+# `pytest`
+# from there, pytest will run any function with the word "test" in it.
+# Print statements are only output if the test fails.
+
 # TODO: Create test for obviously distinct groups 
 from pprint import pprint
 import em
 import numpy as np
 
-test_points = [[1,50,100], [2, 60, 300], [5, 80, 500], [7, 40, 400]]
-test_distributions = em.init_random_distributions(3, test_points)
+test_points = [[1.27,50.367,100.4], [2, 60.2, 300], [-5, 74, 534], [7, 0, 470]]
+test_distributions = em.init_random_distributions(2, test_points)
 
 
 def test_init_dist():
     dists = em.init_random_distributions(3, [[1,50,100], [2, 60, 300], [5, 80, 500], [7, 40, 400]])
     pprint(dists, width=1)
+    pprint(test_points)
+    # assert False
 
 
 def test_expectation():
     prob_mtx = em.expectation(test_distributions, test_points)
     pprint(prob_mtx, width = 1)
+    assert np.allclose(np.sum(prob_mtx, axis=1), np.ones(prob_mtx.shape[0]))
+
 
 def test_point_clust_prob():
+    pprint(test_points[0])
+    pprint(test_distributions[0])
+    pprint(test_distributions)
     prob = em.point_cluster_probability(test_points[0], test_distributions[0], test_distributions)
     pprint(prob)
+    # assert False
+
+
+def test_maximization():
+    dists = em.maximization(em.expectation(test_distributions, test_points), test_points)
+    pprint(dists)
+    # assert False
+
+
+def test_max_mean():
+    mean = em.maximize_mean(1.0/3, em.expectation(test_distributions, test_points), 0, test_points)
+    pprint(mean)
     # assert False