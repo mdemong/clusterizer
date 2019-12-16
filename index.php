9f7bdb5f72957b5f7d5c2236149aafe4a63ae90b
Gurdev Sihra <sihragurdev@gmail.com>
2019-12-15 19:18:31 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -63,11 +63,37 @@
             <h3> <br>Micah Demong | Gurdev Sihra | Katrina Tran</h3>
         </div>
         <div>
-            <h1>k-means clustering</h1>
-                <h3>...information about k-means clustering here...<h3>
-            <h1>expectation maximization</h1>
-                <h3>...information about expectation maximization here...<h3>
-            <h1>want to try it out?</h1>
+             <div class = "text-center"><h1>K-Means Clustering Algorithm</h1></div>
+    <div class = "text-center"><img class="rounded" src="https://upload.wikimedia.org/wikipedia/commons/e/ea/K-means_convergence.gif" alt="" width=30% height=30%;></div>
+    <h3>An iterative algorithm that tries to partition the dataset into K pre-defined distinct non-overlapping subgroups (clusters) where each data point belongs to only one group. This algorithm is implemented using the following steps:<h3>
+              <h4>    <ol>
+                <li>Specify number of clusters K.</li>
+                <li>Initialize centroids by first shuffling the dataset and then randomly selecting K data points for the centroids without replacement.</li>
+                <li>Keep iterating until there is no change to the centroids. The centroids are updated like:</li>
+                </ol></h4>
+  
+             <h5>   <ul>
+                <li>Compute the sum of the squared distance between data points and all centroids.</li>
+                <li>Assign each data point to the closest cluster (centroid).</li>
+                <li>Compute the centroids for the clusters by taking the average of the all data points that belong to each cluster.</li>
+                </ul>
+    </h5>
+        <h4>Learn more at<a href ="https://towardsdatascience.com/k-means-clustering-algorithm-applications-evaluation-methods-and-drawbacks-aa03e644b48a"> this link</a>!</h4>
+    
+     <div class = "text-center"><h1>Expectation Maximization Algorithm</h1></div>
+    <div class = "text-center"><img class="rounded" src="https://upload.wikimedia.org/wikipedia/commons/6/69/EM_Clustering_of_Old_Faithful_data.gif" alt=""></div>
+    <h3>An algorithm that uses the available observed data of the dataset to estimate the missing data and then using that data to update the values of the parameters. This is implemented in the following steps:<h3>
+    <ol>
+    <h4>  <li>Initially, a set of initial values of the parameters are considered. A set of incomplete observed data is given to the system with the assumption that the observed data comes from a specific model.</li>
+    <li>The next step is known as “Expectation” – step or E-step. In this step, we use the observed data in order to estimate or guess the values of the missing or incomplete data. It is basically used to update the variables.</li>
+    <li>The next step is known as “Maximization”-step or M-step. In this step, we use the complete data generated in the preceding “Expectation” – step in order to update the values of the parameters. It is basically used to update the hypothesis.</li>
+    <li>Now, in the fourth step, it is checked whether the values are converging or not, if yes, then stop otherwise repeat step-2 and step-3 i.e. “Expectation” – step and “Maximization” – step until the convergence occurs.</li>
+    </ol>
+    </h4>
+    <h4>Learn more at<a href ="https://www.geeksforgeeks.org/ml-expectation-maximization-algorithm/"> this link</a>!</h4>
+    
+    
+    <h1>Want to try it out?</h1>
                 <h3><a href = "signup.php">sign up here!</a><h3>
         </div>
     </div>
