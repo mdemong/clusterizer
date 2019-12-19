1f537d9468cab6a020315db2a46c58aa59b5b0d2
mdemong <mdemong1@gmail.com>
2019-12-18 21:04:37 -0800

    Updated the magic configuration file

--- magic.php
+++ magic.php
@@ -1,6 +1,22 @@
 <?php
-	$hn = 'localhost';
-	$un = 'otherme';
-	$pw = 'sbS?}[D$,cr^8=a';
-	$db = 'clusterizer';
+
+    // magic.php - Configuration file
+    // 
+    // This file contains configuration information for the MySQL database.
+    // Please modify this information with the appropriate information for
+    // your MySQL setup.
+    //
+    // $hn: the hostname of your database (default: localhost)
+    // $db: the name of the MySQL database (default: clusterizer)
+    // $un: the MySQL username associated with your program
+    // $pw: the MySQL password associated with your program
+
+
+    $hn = 'localhost';
+    $db = 'clusterizer';
+
+    // These values need to be set appropriately.
+    $un = '';
+    $pw = '';
+
 ?>
